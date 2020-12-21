813daf22388ec213a5920746404699c94289cdb8
Shaheen Gandhi <shaheen@fb.com>
2020-12-20 17:57:14 -0800

    Update build.sh to use cargo +nightly

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+# When building using a standard/nightly toolchain, +nightly -Z build-std must be used
+cargo +nightly build -Z build-std --target x86_64-apple-ios-macabi $*
+
+# When building with a local toolchain, it's not necessary since the local toolchain build would also build std
+#cargo build --target x86_64-apple-ios-macabi $*
