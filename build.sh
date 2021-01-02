6570694b4d19b36bd133e85633aed9be97d1497d
Shaheen Gandhi <shaheen@fb.com>
2021-01-02 00:22:43 -0800

    Format build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
 
-# When building using a standard/nightly toolchain, +nightly -Z build-std must be used
+# When building using a standard/nightly toolchain, +nightly -Z build-std must
+# be used
 #cargo +nightly build -Z build-std --target x86_64-apple-ios-macabi $*
 
-# When building with a local toolchain, it's not necessary since the local toolchain build would also build std
-cargo build --target x86_64-apple-ios-macabi $*
+# When building with a local toolchain, it's not necessary since the local
+# toolchain build can include std (and only nightly cargo supports -Z
+# build-std).
+cargo build $*
