786746be1ba8f4477e59827d3bc0989951a971a2
Shaheen Gandhi <shaheen@fb.com>
2021-01-02 00:23:25 -0800

    Add clap and options parsing

--- src/server.rs
+++ src/server.rs
@@ -1,3 +1,4 @@
+use clap::{Arg, App, crate_version, crate_description};
 use tonic::transport::Server;
 use tokio;
 
@@ -5,6 +6,11 @@ mod hkservice;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let matches = App::new("HKServer")
+        .version(crate_version!())
+        .about(crate_description!())
+        .get_matches();
+
     let addr: std::net::SocketAddr = "127.0.0.1:55123".parse().unwrap();
     let service = hkservice::HKServer::new();
     Server::builder()
