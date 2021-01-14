c29f1971b15df518c34a1c6724eb5bbbcbe45015
Shaheen Gandhi <shaheen@fb.com>
2021-01-13 23:35:17 -0800

    Codesign with HomeKit entitlements

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+set -e
+set -x
+
 # When building using a standard/nightly toolchain, +nightly -Z build-std must
 # be used
 #cargo +nightly build -Z build-std --target x86_64-apple-ios-macabi $*
@@ -12,3 +15,9 @@ cargo build $*
 # Use cargo-bundle to build and package the .app for the server. It requires
 # using a --target argument regardless of whether .cargo/config.toml exists.
 cargo bundle --target x86_64-apple-ios-macabi --bin server $*
+
+# Code sign
+codesign --entitlements entitlements.plist -s \
+         $(security find-identity -v -p codesigning | awk '{print $2}' | head -n 1) \
+         ./target/x86_64-apple-ios-macabi/debug/bundle/osx/hkserver.app/Contents/MacOS/server
+
