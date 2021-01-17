9da87bbb42e4ea91b21d6a37886ebdd6fd72acea
Shaheen Gandhi <shaheen@fb.com>
2021-01-17 11:56:08 -0800

    Snapshot Xcode project

---
+++ HKServer/HKServer/HKServerApp.swift
@@ -0,0 +1,17 @@
+//
+//  hkserverApp.swift
+//  hkserver
+//
+//  Created by Shaheen Gandhi on 1/17/21.
+//
+
+import SwiftUI
+
+@main
+struct hkserverApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
