8185956661b17d3ba9356b16fde8ffebfff3e1d1
Shaheen Gandhi <shaheen@fb.com>
2021-01-18 16:13:34 -0800

    Make HKServer a command line app and integrate ArgumentParser

--- HKServer/HKServer/HKServerApp.swift
+++ HKServer/HKServer/HKServerApp.swift
@@ -5,13 +5,17 @@
 //  Created by Shaheen Gandhi on 1/17/21.
 //
 
-import SwiftUI
+import ArgumentParser
 
 @main
-struct hkserverApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+struct hkserverApp : ParsableCommand {
+    @Flag(help: "Verbose logging")
+    var verbose = false
+
+    @Option(name: .shortAndLong, help: "Port to bind to. Default is 20000")
+    var port: Int?
+
+    mutating func run() throws {
+        port = port ?? 20000
     }
 }
