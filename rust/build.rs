c58cf8391984baf5dc75dc45d074b436092931ed
Shaheen Gandhi <shaheen@fb.com>
2021-01-18 16:43:24 -0800

    Move proto file to shared location

--- rust/build.rs
+++ rust/build.rs
@@ -3,7 +3,7 @@ use std::path::{Path, PathBuf};
 use tonic_build;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::compile_protos("protos/hkserver.proto")?;
+    tonic_build::compile_protos("../protos/hkserver.proto")?;
 
     // Inject build project as cfg "profile" key
     println!("cargo:rustc-cfg=profile=\"{}\"", std::env::var("PROFILE").unwrap());
