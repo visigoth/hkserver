f81a766e9bcfc5d5e0500115f62e26622c058d56
Shaheen Gandhi <shaheen@fb.com>
2021-01-21 14:55:54 -0800

    Add build script for HKServer

---
+++ HKServer/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+
+# First build swift grpc plugins
+pushd third-party/grpc-swift && make plugins && popd
+
+# Ensure protoc and plugins are on the path
+export PATH=$(pwd)/third-party/protoc/bin:$(pwd)/third-party/grpc-swift:$PATH
+
+# Generate protobuf and grpc code
+mkdir -p gen
+protoc --proto_path=../protos --swift_out=HKServer ../protos/hkserver.proto
+protoc --proto_path=../protos --grpc-swift_out=HKServer ../protos/hkserver.proto
+
+# Build the project
+xcodebuild -workspace hkserver.xcworkspace -scheme hkserver
+
