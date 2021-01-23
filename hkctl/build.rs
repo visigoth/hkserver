babc0333b97684e66d24c64bf0850c8652cce5f2
Shaheen Gandhi <shaheen@fb.com>
2021-01-22 23:20:07 -0800

    Use build-deps to register build changes

--- hkctl/build.rs
+++ hkctl/build.rs
@@ -1,7 +1,9 @@
+use build_deps;
 use tonic_build;
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::compile_protos("../protos/hkserver.proto")?;
+    build_deps::rerun_if_changed_paths("../protos/hkserver.proto").unwrap();
 
     // Inject build project as cfg "profile" key
     println!("cargo:rustc-cfg=profile=\"{}\"", std::env::var("PROFILE").unwrap());
