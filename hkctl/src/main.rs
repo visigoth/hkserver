40c72364c7a39d1dcb03d618e9ce68004e116aa1
Shaheen Gandhi <shaheen@fb.com>
2021-01-22 23:20:45 -0800

    Refactor main.rs

--- hkctl/src/main.rs
+++ hkctl/src/main.rs
@@ -1,47 +1,63 @@
 mod hkservice;
 mod homes;
 
-use clap::{App, Arg, crate_version, value_t};
-use tonic::transport::Uri;
+use clap::{App, Arg, crate_version};
+use tonic::transport::{Channel, Uri};
 use tokio;
 use hkservice::home_kit_service_client::HomeKitServiceClient;
+use std::error::Error;
+
+impl HomeKitServiceClient<Channel> {
+    async fn create(host: &str, port: u32) -> Result<HomeKitServiceClient<Channel>, Box<dyn Error>> {
+        let authority = format!("{}:{}", host, port);
+        let endpoint = Uri::builder()
+            .scheme("http")
+            .authority(authority.as_str())
+            .path_and_query("/")
+            .build();
+        let channel = tonic::transport::Channel::builder(endpoint.unwrap())
+            .connect()
+            .await?;
+        Ok(HomeKitServiceClient::new(channel))
+    }
+}
 
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let matches = App::new("hkctl")
+async fn main() -> Result<(), Box<dyn Error>> {
+    let mut app = App::new("hkctl")
         .version(crate_version!())
         .about("Command line porcelain for HomeKit")
-        .arg(Arg::with_name("v")
-             .short("c")
+        .arg(Arg::new("v")
+             .short('c')
              .multiple(true)
-             .help("Sets verbosity"))
-        .arg(Arg::with_name("port")
-             .short("p")
-             .help("Local port to connect to"))
-        .arg(Arg::with_name("home")
-             .help("Specify a home. Defaults to the primary home"))
+             .about("Sets verbosity"))
+        .arg(Arg::new("port")
+             .short('p')
+             .about("Local port to connect to"))
+        .arg(Arg::new("home")
+             .about("Specify a home. Defaults to the primary home"))
         .subcommand(App::new("homes")
-                    .about("Lists homes"))
-        .get_matches();
+                    .about("Lists homes"));
 
-    let port = value_t!(matches, "port", u32).unwrap_or(55123);
-    let authority = format!("127.0.0.1:{}", port);
-    let endpoint = Uri::builder()
-        .scheme("http")
-        .authority(authority.as_str())
-        .path_and_query("/")
-        .build();
-    let channel = tonic::transport::Channel::builder(endpoint.unwrap())
-        .connect()
-        .await?;
-    let client = HomeKitServiceClient::new(channel);
+    let matches = app.get_matches_mut();
+    let port = match matches.value_of_t::<u32>("port") {
+        Ok(port) => port,
+        Err(e) => {
+            if e.kind == clap::ErrorKind::ArgumentNotFound {
+                55123
+            } else {
+                e.exit()
+            }
+        }
+    };
+    let client = HomeKitServiceClient::create("127.0.0.1", port).await?;
 
     match matches.subcommand_name() {
         Some("homes") => {
             homes::command(matches.subcommand_matches("homes").unwrap(), client).await.unwrap();
         },
         _ => {
-            println!("oijasdoj");
+            app.print_help()?
         }
     }
     Ok(())
