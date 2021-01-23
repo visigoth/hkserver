d99f3097efe5099be24ccdfd769dc95da534c8ac
Shaheen Gandhi <shaheen@fb.com>
2021-01-23 00:07:26 -0800

    Better formatting for homes output

--- hkctl/src/homes.rs
+++ hkctl/src/homes.rs
@@ -1,7 +1,21 @@
 use clap::{ArgMatches};
 use tonic::transport::Channel;
 use crate::hkservice::home_kit_service_client::HomeKitServiceClient;
-use crate::hkservice::EnumerateHomesRequest;
+use crate::hkservice::{EnumerateHomesRequest, EnumerateHomesResponse};
+use crate::hkservice::home_information::HomeHubState;
+
+fn print_response(response: &EnumerateHomesResponse) {
+    response.homes.iter().for_each(|home| {
+        let primary = if home.is_primary { " (Primary)" } else { "" };
+        let hub_state = match HomeHubState::from_i32(home.hub_state).unwrap() {
+            HomeHubState::Connected => "Connected",
+            HomeHubState::Disconnected => "Disconnected",
+            HomeHubState::NotAvailable => "Not Available",
+        };
+        println!("Home: {}{}", home.name, primary);
+        println!("  Hub State: {}", hub_state);
+    });
+}
 
 pub async fn command(_matches: &ArgMatches, mut client: HomeKitServiceClient<Channel>) -> Result<(), Box<dyn std::error::Error>> {
     let response = client.enumerate_homes(
@@ -9,6 +23,6 @@ pub async fn command(_matches: &ArgMatches, mut client: HomeKitServiceClient<Cha
             name_filter: String::from("")
         }
     ).await?.into_inner();
-    println!("RESPONSE={:?}", response);
+    print_response(&response);
     Ok(())
 }
